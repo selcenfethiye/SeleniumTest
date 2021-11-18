6249b830caddc44f948bca4db1314872bf17e0a7
selcenfethiye <93594827+selcenfethiye@users.noreply.github.com>
2021-11-18 23:56:13 +0300

    Update TestN11.java

--- TestN11.java
+++ TestN11.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test2 {
+import java.sql.SQLOutput;
+
+public class TestN11 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\selce\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -13,12 +15,23 @@ public class test2 {
         driver.get("https://www.n11.com/");
         driver.manage().window().maximize();
 
+        String title = driver.getTitle();
+
         WebElement search = driver.findElement(By.id("searchData"));
         search.click();
         search.sendKeys("bilgisayar");
 
         WebElement searchButton = driver.findElement(new By.ByCssSelector(".searchBtn"));
         searchButton.click();
+
+        String title2 = driver.getTitle();
+
+        if(title != title2){
+            System.out.println("Test başarılı!");
+        }
+        else{
+            System.out.println("Test başarısız!");
+        }
 
         driver.quit();
 
